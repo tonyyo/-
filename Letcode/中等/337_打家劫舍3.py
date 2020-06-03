a28620923336c352103858e0ccfc4117d1c4ea01
vincent <wenxing.zhu@transwarp.io>
2020-06-03 20:57:38 +0800

    6.3.2 in transwarp

--- Letcode/中等/337_打家劫舍3.py
+++ Letcode/中等/337_打家劫舍3.py
@@ -6,3 +6,14 @@ class TreeNode:
 
 class Solution:
     def rob(self, root: TreeNode) -> int:
+        Y, N = self.subRob(root)
+        return max(Y, N)
+
+    def subRob(self, root):  # 从root节点进入 偷root节点和不偷root节点返回的最大金额
+        if root == None:
+            return 0, 0
+        leftY, leftN = self.subRob(root.left)
+        rightY, rightN = self.subRob(root.right)
+        Y = leftN + rightN + root.val
+        N = max(leftY, leftN) + max(rightN, rightY)
+        return Y, N
